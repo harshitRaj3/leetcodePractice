6f9580de2df9587d9d3668c607f29696532fd438
harshitRaj3 <itsharshit.raj3@gmail.com>
2026-03-08 20:04:29 +0530

    Time: 0 ms (100.00%) | Memory: 53.1 MB (35.19%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,29 +1,20 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        if(nums.length==1) return nums[0];
-        if(nums[0]!=nums[1]) return nums[0];
-        if(nums[nums.length-1]!=nums[nums.length-2]) return nums[nums.length-1];
-        int low=0;
-        int high=nums.length-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
-                return nums[mid];
-            }
-            int f=mid,se=mid;
-            if(nums[mid-1]==nums[mid]){
-                f=mid-1;
-            }
-            else if(nums[mid]==nums[mid+1]){
-                se=mid+1;
-            }
-            int leftcount=f-low;
-            int rightcount=high-se;
-            if(leftcount%2==0){
-                low=se+1;
-            }
-            else high=f-1;
+       if(nums.length==1) return  nums[0];
+       if(nums[0]!=nums[1]) return nums[0];
+       if(nums[nums.length-1]!=nums[nums.length-2]) return nums[nums.length-1];
+     int low=1;
+     int high=nums.length-2;
+     while(low<=high){
+        int mid=(low+high)/2;
+        if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]) return nums[mid];
+        if((mid%2==1 && nums[mid-1]==nums[mid])||(mid%2==0 && nums[mid]==nums[mid+1])){
+            low=mid+1;
         }
+           else high=mid-1;
+
+     }
+        
       return 1;  
     }
 }
