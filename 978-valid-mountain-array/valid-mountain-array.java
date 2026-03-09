019d798008b76739f46478a7012bc2c002dc9dab
harshitRaj3 <itsharshit.raj3@gmail.com>
2026-03-09 20:14:13 +0530

    Time: 1 ms (99.89%) | Memory: 47 MB (99.26%) - LeetSync

---
+++ 978-valid-mountain-array/valid-mountain-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean validMountainArray(int[] nums) {
+        if(nums.length<3) return false;
+         int i=0;
+         int n=nums.length;
+         while(i+1<n && nums[i]<nums[i+1]){
+            i++;
+         }
+         if(i==0||i==n-1){
+            return false;
+         }
+         while(i+1<n && nums[i]>nums[i+1]){
+            i++;
+         }
+         return i==n-1;
+         
+    }
+}
