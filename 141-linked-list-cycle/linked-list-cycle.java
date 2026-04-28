43be8dd92b98cf33356cc522eb949ceef0723c89
harshitRaj3 <itsharshit.raj3@gmail.com>
2026-04-28 19:16:03 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (16.45%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -16,9 +16,9 @@ public class Solution {
         while(fast!=null && fast.next!=null){
             slow=slow.next;
             fast=fast.next.next;
-            if(fast==slow) return true;
+         if(slow==fast) return true;
         }
-     return false;
+ return false;
         
     }
 }
